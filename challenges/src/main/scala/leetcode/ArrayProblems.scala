7bcba353010c590e20e4e26fc463c786a91d5d7d
Dexter <dexter2305@gmail.com>
2024-04-19 02:07:57 +0530

    Completed daily challenge.

--- challenges/src/main/scala/leetcode/ArrayProblems.scala
+++ challenges/src/main/scala/leetcode/ArrayProblems.scala
@@ -33,25 +33,84 @@ object ArrayProblems:
     * Constraints:
     *
     * n == tickets.length 1 <= n <= 100 1 <= tickets[i] <= 100 0 <= k < n
+    *
+    * Approach
+    *
+    * Solution to be approached in a single pass Every ticket bought is ONE second. Question is to
+    * find the time required. Time required will be equal to the number of tickets sold until kth
+    * person has no more tickets to buy.
+    *
+    * Everyone in front of k will either buy k or less than k. Everyone after k will buy on
+    * tickets(k) - 1 tickets as the counting stops after k == 0.
     */
   def timeRequiredToBuy(tickets: Array[Int], k: Int): Int =
-    /*
-     * Solution to be approached in a single pass
-     * Every ticket bought is ONE second. Question is to find the time required. Time required will be equal to the number of tickets sold until kth person has no more tickets to buy.
-     *
-     * Everyone in front of k will either buy k or less than k.
-     * Everyone after k will buy on tickets(k) - 1 tickets as the counting stops after k == 0.
-     *
-     **/
-    // var res = 0
-    // for i <- 0 until tickets.length
-    // do
-    //   if i <= k then res = res + math.min(tickets(i), tickets(k))
-    //   else res = res + math.min(tickets(i), tickets(k) - 1)
-    // res
     (0 until tickets.length).foldLeft(0): (acc, index) =>
-      val x =
-        if index <= k then tickets(index).min(tickets(k)) + acc
-        else tickets(index).min(tickets(k) - 1) + acc
-      // println(s"for ${tickets.mkString("-")} at index = $index, tickets(index) = ${tickets(index)} vs ${tickets(k)}, x = $x")
-      x
+      if index <= k then tickets(index).min(tickets(k)) + acc
+      else tickets(index).min(tickets(k) - 1) + acc
+
+    /** 463. Island perimeter.
+      *
+      * You are given row x col grid representing a map where grid[i][j] = 1 represents land and
+      * grid[i][j] = 0 represents water.
+      *
+      * Grid cells are connected horizontally/vertically (not diagonally). The grid is completely
+      * surrounded by water, and there is exactly one island (i.e., one or more connected land
+      * cells).
+      *
+      * The island doesn't have "lakes", meaning the water inside isn't connected to the water
+      * around the island. One cell is a square with side length 1. The grid is rectangular, width
+      * and height don't exceed 100. Determine the perimeter of the island.
+      *
+      * Example 1:
+      *
+      * Input: grid = [[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]] Output: 16 Explanation: The
+      * perimeter is the 16 yellow stripes in the image above.
+      *
+      * Example 2:
+      *
+      * Input: grid = [[1]] Output: 4
+      *
+      * Example 3:
+      *
+      * Input: grid = [[1,0]] Output: 4 Constraints:
+      *
+      * row == grid.length col == grid[i].length 1 <= row, col <= 100 grid[i][j] is 0 or 1. There is
+      * exactly one island in grid.
+      */
+  def islandPerimeter(grid: Array[Array[Int]]): Int =
+
+    val rRange = 0 until grid.length
+    val cRange = 0 until grid(0).length
+
+    /* pretty print given grid with a comment (debugging purpose)*/
+    def pprint(grid: Array[Array[Int]], comment: Option[String] = None): Unit =
+      val values = (for r <- rRange
+      yield (for c <- cRange
+      yield s"${grid(r)(c)}").toList).toList
+      comment match
+        case None => println("Matrix !!")
+        case Some(string) =>
+          println(string)
+          println("*" * string.length())
+      values.foreach: list =>
+        print(list.mkString(" "))
+        println
+
+    /* given r,c - returns number of neighbouring islands */
+    def neighboursFor(row: Int, col: Int): Int =
+      val neighbours = List((row, col - 1), (row, col + 1), (row - 1, col), (row + 1, col))
+      val present = neighbours
+        .filter: (x, y) =>
+          rRange.contains(x) && cRange.contains(y)
+        .map(point => grid(point._1)(point._2))
+        .sum
+      present
+
+    // pprint(grid = grid, Option("Given input"))
+    val ps =
+      for r <- rRange
+      yield for c <- cRange
+      yield
+        if grid(r)(c) == 0 then 0
+        else 4 - neighboursFor(r, c)
+    ps.map(_.sum).sum
--- challenges/src/test/scala/leetcode/ArrayProblemsFunsuite.scala
+++ challenges/src/test/scala/leetcode/ArrayProblemsFunsuite.scala
@@ -11,3 +11,13 @@ class ArrayProblemsFunsuite extends AnyFunSuite with ScalaCheckPropertyChecks:
     )
     forAll(testcases): (tickets, k, expected) =>
       assert(ArrayProblems.timeRequiredToBuy(tickets, k) === expected)
+
+  test("463. Island perimter"):
+    val testcases = Table(
+      ("grid", "expected perimeter"),
+      (Array(Array(0, 1, 0, 0), Array(1, 1, 1, 0), Array(0, 1, 0, 0), Array(1, 1, 0, 0)), 16),
+      (Array(Array(1)), 4),
+      (Array(Array(1, 0)), 4)
+    )
+    forAll(testcases): (grid, expected) =>
+      assert(ArrayProblems.islandPerimeter(grid) === expected)
