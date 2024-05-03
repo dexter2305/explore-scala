dc72ba42ad36aea0ebe6bf29607e2f83c0a30016
Dexter <dexter2305@gmail.com>
2024-05-03 23:20:17 +0530

    Completed one matrix problem.

---
+++ challenges/src/main/scala/leetcode/MatrixProblems.scala
@@ -0,0 +1,57 @@
+package leetcode
+
+/** Matrix problems */
+object MatrixProblems:
+
+  /** 1572. Matrix diagonal sum.
+    *
+    * Given a square matrix mat, return the sum of the matrix diagonals.
+    *
+    * Only include the sum of all the elements on the primary diagonal and all the elements on the secondary diagonal that are not part of the primary diagonal.
+    *
+    * Example 1:
+    *
+    * - Input: mat = [[1,2,3],
+    *                 [4,5,6],
+    *                 [7,8,9]]
+    * - Output: 25
+    * - Explanation: Diagonals sum: 1 + 5 + 9 + 3 + 7 = 25
+    * Notice that element mat[1][1] = 5 is counted only once.
+    *
+    * Example 2:
+    *
+    * - Input: mat = [[1,1,1,1],
+    *                 [1,1,1,1],
+    *                 [1,1,1,1],
+    *                 [1,1,1,1]]
+    * - Output: 8
+    *
+    * Example 3:
+    *
+    * - Input: mat = [[5]]
+    * - Output: 5
+    *
+    * Constraints:
+    *
+    *  - n == mat.length == mat[i].length
+    *  - 1 <= n <= 100
+    *  - 1 <= mat[i][j] <= 100
+    *
+    * ### Approach
+    *
+    * - We dont need a nested for loop. Every iteration two elements will get summed up. Each of those elements belong to two diagonals.
+    * - Elements from first diagonal come from (i, i), where i is row number.
+    * - Second diagonal is tricky. Matrix is to determine the formula for second diagonal elements.
+    *
+    * | *(0,0)* | (0,1)   | *(0,2)* | (0, n-1)
+    * | (1,0)   | *(1,1)* | (1,2)   | (1, (n-1) -1)
+    * | *(2,0)* | (2,1)   | *(2,2)* | (2, (n-1) -2)
+    *
+    * So, the generic formula would be `(i, n-1-i)` where i is row and n is the total column count.
+    * - Finally if the diagonals intersect at a cell, that cell will get counted twice. This happens when column count is odd. Adjust sum accordingly.
+    */
+
+  def diagonalSum(mat: Array[Array[Int]]): Int =
+    val gt =
+      (for i <- mat.indices yield mat(i)(i) + mat(i)(mat.length - i - 1)).sum
+    if mat.length % 2 == 0 then gt else gt - mat(mat.length / 2)(mat.length / 2)
---
+++ challenges/src/test/scala/leetcode/MatrixProblemsFunsuite.scala
@@ -0,0 +1,26 @@
+package leetcode
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+
+class MatrixProblemsFunsuite extends AnyFunSuite with ScalaCheckPropertyChecks:
+  test("1572. Matrix diagonal sum."):
+    val testcases = Table(
+      ("matrix", "expected sum"),
+      (
+        List(
+          List(1, 2, 3),
+          List(4, 5, 6),
+          List(7, 8, 9)
+        ),
+        25
+      ),
+      (
+        List(List(1, 1, 1, 1), List(1, 1, 1, 1), List(1, 1, 1, 1), List(1, 1, 1, 1)),
+        8
+      ),
+      (List(List(5)), 5)
+    )
+    forAll(testcases): (matrix, expected) =>
+      assertResult(expected):
+        MatrixProblems.diagonalSum(matrix.map(_.toArray).toArray)
